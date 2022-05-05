526fced825fe49386d0a1cc2c955ca3fe56c6f25
shaunvan119 <shaunvan@live.com.au>
2022-05-06 09:35:53 +1000

    badgeText varible

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,18 @@ import star from '../assets/star.png'
 
 
 export default function Card(props) {
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+
     return (
     <div className="card">
+        {badgeText && <div className="card--badge">{badgeText}</div>}
            <img src={katie} alt="Katie-pic" className="card--image" />
             <div className="card--stats">
                 <img src={star} alt="star-pic" className="card--star" />
