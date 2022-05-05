57a7789802e68f2aa766f3f9edafb66f09f51054
shaunvan119 <shaunvan@live.com.au>
2022-05-05 15:57:28 +1000

    rendering data

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 
 
 export default function App() {
+           
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
     return (
-        <div className="container">
+        <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="katie.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cards}
         </div>
     )
 }
