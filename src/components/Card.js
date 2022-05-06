dc163eb91ca40d6200dbbde3142939e711bdb02e
shaunvan119 <shaunvan@live.com.au>
2022-05-06 10:04:26 +1000

    refactored item props

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,9 +18,9 @@ export default function Card(props) {
            <img src={katie} alt="Katie-pic" className="card--image" />
             <div className="card--stats">
                 <img src={star} alt="star-pic" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
